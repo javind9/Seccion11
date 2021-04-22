e1cb91c1b9abc54ed3318dcb6a9b390363836558
Javier <jndominguez@isotrol.com>
2021-04-22 14:20:03 +0200

    195

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { getMaxListeners } from 'process';
+import { PaisService } from 'src/app/services/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -15,9 +16,13 @@ export class TemplateComponent implements OnInit {
     correo: 'javier@gmail.com'
   }
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.paisService.getPaises()
+      .subscribe( paises =>{
+        console.log(paises);
+      })
   }
 
   guardar( forma: NgForm ){
